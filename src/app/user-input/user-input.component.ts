db63e43465e6b0f03737108527a5aa62cbe00af6
Ryan White <mrwhiterk@gmail.com>
2026-06-06 17:59:48 -0400

    scaffolded user-input

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';
 
+
+  onSubmit(){
+    console.log('Form submitted!');
+    console.log('Initial Investment:', this.enteredInitialInvestment);
+    console.log('Annual Investment:', this.enteredAnnualInvestment);
+    console.log('Expected Return:', this.enteredExpectedReturn);
+    console.log('Duration:', this.enteredDuration);
+  }
 }
