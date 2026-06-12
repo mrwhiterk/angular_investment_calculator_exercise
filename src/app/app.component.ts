24e9ff3b5bbad206e3bd2cf2b0efd45daa40641d
Ryan White <mrwhiterk@gmail.com>
2026-06-12 18:03:44 -0400

    90. migrating to angular modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,7 @@
 import { Component, signal } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { UserInputComponent } from './user-input/user-input.component';
-import { InvestmentResultsComponent } from './investment-results/investment-results.component';
-import { InvestmentInput } from './investment-input.model';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-  
-  // resultsData = signal<{
-  //   year: number;
-  //   interest: number;
-  //   valueEndOfYear: number;
-  //   annualInvestment: number;
-  //   totalInterest: number;
-  //   totalAmountInvested: number;
-  // }[] | undefined>(undefined);
-
-  
-}
+export class AppComponent {}
--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,43 +1,12 @@
 import { Component, computed, inject, Input, input } from '@angular/core';
-import { CurrencyPipe } from '@angular/common';
 import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-results',
-  standalone: true,
-  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css',
 })
 export class InvestmentResultsComponent {
-
-  // constructor (private investmentService: InvestmentService) {}
-  // or
   private investmentService = inject(InvestmentService);
-
-  // get results() {
-  //   return this.investmentService.resultData;
-  // }
-  // or using signals
-  // results = computed(() => this.investmentService.resultData());
-  // or 
   results = this.investmentService.resultData.asReadonly();
-  
-  // results = input<{
-  //   year: number;
-  //   interest: number;
-  //   valueEndOfYear: number;
-  //   annualInvestment: number;
-  //   totalInterest: number;
-  //   totalAmountInvested: number;
-  // }[]>() // using signals
-
-  // @Input() results?: {
-  //   year: number;
-  //   interest: number;
-  //   valueEndOfYear: number;
-  //   annualInvestment: number;
-  //   totalInterest: number;
-  //   totalAmountInvested: number;
-  // }[]; // array of objects with the same structure as the one we created in app.component.ts
 }
